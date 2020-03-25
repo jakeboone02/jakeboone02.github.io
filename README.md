b4f6789a7bad063c52061db8012dd2161e89d471
Jake Boone <jakeboone02@gmail.com>
2020-03-25 08:33:56 -0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-https://jakeboone02.github.io
-
-https://www.npmjs.com/~jakeboone02
---
+++ precache-manifest.48247e5ad5f40254739a9e64edb10730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e531b8d7735fb00ffdbe65221748ab59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63f7f9d9153e61937a96",
+    "url": "/static/css/main.f74f4b77.chunk.css"
+  },
+  {
+    "revision": "c8fd65f4ae1fbb474f7b",
+    "url": "/static/js/2.8f8f3339.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.8f8f3339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f7f9d9153e61937a96",
+    "url": "/static/js/main.7d309e94.chunk.js"
+  },
+  {
+    "revision": "b9fe77ecd0df8512f96a",
+    "url": "/static/js/runtime-main.00456292.js"
+  }
+]);
